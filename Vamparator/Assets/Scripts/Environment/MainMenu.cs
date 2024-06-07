842b30c6e1b7b62e8fba4c8c1fe4b91ab829b639
AnyPerson1 <arda45simsek@gmail.com>
2024-06-07 04:57:13 +0300

    EndGame

--- Vamparator/Assets/Scripts/Environment/MainMenu.cs
+++ Vamparator/Assets/Scripts/Environment/MainMenu.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] GameObject fadeImage;
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] Slider slider;
+    private void Update()
+    {
+        audioSource.volume = slider.value;
+    }
     public void startGame()
     {
         StartCoroutine(fade());
